9606f58c95ecd936c1ce6d9e06655e1df0a3a5a6
Pierre Sakagne Ndione <111321600+pierre1717@users.noreply.github.com>
2025-11-13 14:55:17 +0000

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const api = axios.create({
-        baseURL: import.meta.env.VITE_API_URL
-        })
-  const [message, setMessage] = useState('Chargement application Moderne ...')
-  
+    baseURL: import.meta.env.VITE_API_URL
+  });
+
+  const [message, setMessage] = useState('Chargement Application Moderne ...');
 
   useEffect(() => {
-    fetch('/api/hello')
+    fetch(import.meta.env.VITE_API_URL + '/api/hello')
       .then(res => res.json())
       .then(data => setMessage(data.text))
-  }, [])
+      .catch(err => setMessage('Erreur lors du chargement de l\'API'))
+  }, []);
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>{message}</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
